255159e30741177787024379dc268fb405468b46
Goldi Babu <goldibabu647@gmail.com>
2025-10-26 17:47:33 +0530

    RC4 Algorithm

---
+++ rc4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import javax.crypto.*;
+public class rc4 {
+    public static void main(String args[]) throws Exception{
+        Scanner  sc = new Scanner(System.in);
+        System.out.print("Enter plain text: ");
+        String plaintext = sc.nextLine();
+
+        KeyGenerator key = KeyGenerator.getInstance("RC4");// or "ARCFOUR"
+        SecretKey sk = key.generateKey();
+
+        Cipher cipher = Cipher.getInstance("RC4");
+
+        cipher.init(Cipher.ENCRYPT_MODE , sk);
+        byte[] encText = cipher.doFinal(plaintext.getBytes());
+        String encbase64 = Base64.getEncoder().encodeToString(encText);
+        System.out.println("Encrypted Text: " + encbase64);
+
+        cipher.init(Cipher.DECRYPT_MODE , sk);
+        byte[] decText = cipher.doFinal(encText);
+        String decbase64 = new String (decText);
+        System.out.println("Decrypted Text: " + decbase64);
+    }
+
+}
