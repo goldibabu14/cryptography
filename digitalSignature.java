4c21952593d771a0953f0bf530d2888fb0596854
Goldi Babu <goldibabu647@gmail.com>
2025-10-28 12:26:13 +0530

    Digital signature

---
+++ digitalSignature.java
@@ -0,0 +1,29 @@
+import java.security.*;
+import java.util.*;
+import javax.crypto.*;
+
+public class digitalSignature {
+    public static void main(String[] args) throws Exception {
+       String str = "hello";
+       
+       KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+       kpg.initialize(512);
+       KeyPair kp = kpg.generateKeyPair();
+       PublicKey pu = kp.getPublic();
+       PrivateKey pr = kp.getPrivate();
+       
+       Signature sign = Signature.getInstance("SHA256withRSA");
+       sign.initSign(pr);
+       sign.update(str.getBytes());
+       byte[] digitalSignature = sign.sign();
+
+       System.out.println("Sign = " +digitalSignature);
+       str= "hell";
+       Signature verify = Signature.getInstance("SHA256withRSA");
+       verify.initVerify(pu);
+       verify.update(str.getBytes());
+       boolean ver = verify.verify(digitalSignature);
+
+       System.out.println("Verification = " +ver);
+    }
+}
