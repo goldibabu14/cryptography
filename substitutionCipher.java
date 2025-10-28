6e851fb1853851d3b86e10b521f063f0ccf6d9b5
Goldi Babu <goldibabu647@gmail.com>
2025-10-28 16:59:42 +0530

    Substitution cipher

--- substitutionCipher.java
+++ substitutionCipher.java
@@ -27,3 +27,53 @@ public class substitutionCipher {
         System.out.println("Decrypted message :" +decrypted);
     }
 }
+
+
+
+
+
+
+
+// import java.util.*;
+
+// public class substitutionCipher {
+//     public static void main(String[] args) {
+//         String plaintext = "abcdefghijklmnopqrstuvwxyz";
+//         String ciphertext = "qwertyuiopasdfghjklzxcvbnm";
+        
+//         String message = "HelloWorld";  
+        
+//         StringBuilder encrypted = new StringBuilder();
+//         StringBuilder decrypted = new StringBuilder();
+
+//         
+//         for (char c : message.toCharArray()) {
+//             if (Character.isLetter(c)) {
+//                 boolean isUpper = Character.isUpperCase(c);
+//                 char lower = Character.toLowerCase(c);
+//                 int idx = plaintext.indexOf(lower);
+//                 char enc = ciphertext.charAt(idx);
+//                 encrypted.append(isUpper ? Character.toUpperCase(enc) : enc);
+//             } else {
+//                 encrypted.append(c);  
+//             }
+//         }
+
+//         System.out.println("Encrypted message: " + encrypted.toString());
+
+//        
+//         for (char c : encrypted.toString().toCharArray()) {
+//             if (Character.isLetter(c)) {
+//                 boolean isUpper = Character.isUpperCase(c);
+//                 char lower = Character.toLowerCase(c);
+//                 int idx = ciphertext.indexOf(lower);
+//                 char dec = plaintext.charAt(idx);
+//                 decrypted.append(isUpper ? Character.toUpperCase(dec) : dec);
+//             } else {
+//                 decrypted.append(c);
+//             }
+//         }
+
+//         System.out.println("Decrypted message: " + decrypted.toString());
+//     }
+// }
